e87d1a97f6ea4fd9aa014350a5ca861c05c24062
ramoin <rgenestre@hotmail.com>
2019-03-28 16:15:48 +0100

    exo 5

---
+++ exo_05.rb
@@ -0,0 +1,24 @@
+puts "On va compter le nombre d'heures de travail à THP"
+puts "Travail : #{10 * 5 * 11}"
+#on calcule le nombres d'heures en multipliant
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+#on multiplie par 60 pour avoir les minutes
+puts "Et en secondes ?"
+
+puts 10 * 5 * 11 * 60 * 60
+#encore une multiplication simple pour avoir les secondes
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7
+#ici la console nous renvoie false car la comparaison ne se verifie pas
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+#ici on fait le calcul de chaque comparatifs
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+#ici on reprend le meme procede pour faire des verifications
