6f7a59f6bc88d4d8f6e2470f4cc20c6ee4e12f04
ramoin <rgenestre@hotmail.com>
2019-03-28 17:15:50 +0100

    exo 9

---
+++ exo_09.rb
@@ -0,0 +1,8 @@
+puts "c'est quoi ton prénom?" 
+print ">"
+first_name = gets.chomp
+puts "c'est quoi ton nom de famille ?"
+print ">"
+second_name = gets.chomp
+
+puts "bonjour #{first_name} #{second_name}!" 
